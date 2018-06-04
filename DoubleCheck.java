09190a027dce87504e9163e9d0bd08d194341954
namanm97 <naman.mehrotra7@gmail.com>
2018-06-04 09:25:41 +0530

    Add files via upload

---
+++ DoubleCheck.java
@@ -0,0 +1,44 @@
+package singleton;
+
+import java.util.ArrayList;
+
+public class TabWindow
+{
+
+	public static TabWindow sc;
+	ArrayList<String> urls;
+	private TabWindow() {
+		// TODO Auto-generated constructor stub
+		urls=new ArrayList<String>();
+		
+	}
+	
+	public static TabWindow getInstance()
+    {
+		if(sc==null)
+        {
+            synchronized(TabWindow.class)
+            {
+                if(sc==null)
+                {
+                    sc=new TabWindow();
+                }
+            }
+        }
+			
+		return sc;
+	}
+	
+	public void addUrl(String url) {
+		urls.add(url);
+	}
+	
+	public void showUrls() {
+		for(String u:urls) {
+			System.out.println(u);
+		}
+	}
+	
+	
+	
+}
